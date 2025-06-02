aa689d24631f1c43bd3d2d267fe1a66a6a5098be
virzarasydann <virzarasydan@gmail.com>
2025-06-02 12:37:51 +0700

    first commit

---
+++ src/tugas/besar/Controller/AuthController.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tugas.besar.Controller;
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.ResultSet;
+
+/**
+ *
+ * @author VirzaAbsyar
+ */
+public class AuthController {
+    
+}
